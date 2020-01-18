c3682a05d4b54589b727ee74f311b6820822186d
Ali Halawa <ahhalawa@ucdavis.edu>
2020-01-18 11:22:49 -0800

    Initial Commit

---
+++ Hackathon2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hackathon2
+//
+//  Created by Ali Halawa on 1/18/20.
+//  Copyright © 2020 Ali Halawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
